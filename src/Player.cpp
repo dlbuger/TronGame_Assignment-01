115dd3ae0b1d193653b132fb0509dc2d4f63af66
ReCo <zhongxiao0711@gmail>
2019-08-25 03:07:24 +1000

    3:07AM 2019-08-25

--- src/Player.cpp
+++ src/Player.cpp
@@ -36,9 +36,7 @@ bool Player::crash(Player p, vector<array<int, 2>> trace)
         cout<< "Draw." << endl;
         return true;
     }
-    cout<<"["<<position[0]<<", "<<position[1]<<"]"<<endl;
     for (array<int,2> pass:trace){
-        cout<<"["<<pass[0]<<", "<<pass[1]<<"]"<<endl;
         if(position == pass){
             cout << color << " Crashed, Game Over!" << endl;
             return true;
